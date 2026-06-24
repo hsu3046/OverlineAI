008046e4686399953b53decda35ef33946e6d84d
Yu SW Ryan H <hsu3046@gmail.com>
2026-06-24 19:15:14 +0900

    Initial Commit

---
+++ Overline/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Overline
+//
+//  Created by Yu Hitomi on 6/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Overline/OverlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  OverlineApp.swift
+//  Overline
+//
+//  Created by Yu Hitomi on 6/24/26.
+//
+
+import SwiftUI
+
+@main
+struct OverlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
